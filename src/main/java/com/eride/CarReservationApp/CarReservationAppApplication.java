f9154458d0ddfcf936ee58e92994cf442de0efdc
PriyaC <priyacjose@gmail.com>
2019-07-25 19:37:42 -0500

    my first commit

---
+++ src/main/java/com/eride/CarReservationApp/CarReservationAppApplication.java
@@ -0,0 +1,13 @@
+package com.eride.CarReservationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarReservationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarReservationAppApplication.class, args);
+	}
+
+}
